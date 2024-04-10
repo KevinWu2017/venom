deaaeeb82bef19d75d558a33d4499f05d6f1ab7c
cpwu <cpwu2017@126.com>
2024-04-10 15:57:45 +0800

    Update build.sh and prepare.sh scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
 mkdir -p build
 cd build && {
+  echo "Building venom"
   rm -rf *
   cmake .. -DCMAKE_BUILD_TYPE=Release -DCUDA_ARCHS="89" -DBASELINE=OFF -DIDEAL_KERNEL=OFF -DOUT_32B=OFF
   make -j
--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,5 +1,1 @@
 apt install -y libgoogle-glog-dev libboost-all-dev
-git submodule update --init --recursive
-
-cd include/CLASP
-git submodule update --init --recursive
